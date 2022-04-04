e48b51d6da86dd12f919c4ec51bf2186a12de241
huyiqing1 <huyiqing1@gmail.com>
2022-04-04 15:36:28 -0400

    create joke controller

---
+++ src/main/java/practice/springframework/Controllers/JokeController.java
@@ -0,0 +1,22 @@
+package practice.springframework.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import practice.springframework.Services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping("/")
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
